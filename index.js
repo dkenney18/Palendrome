382b6f120953113c9a6d2a0b5a210594fadaa965
Dev K <35117293+dkenney18@users.noreply.github.com>
2021-07-22 19:22:19 +0000

    Formatting

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function pCheck(sentance) {
+  
   //normalize the strings first and reverse one to check if it is the same forwards as well as backwards
+
   let reversed = sentance.split(" ").join("").split("").reverse().join("").toLowerCase()
+
   let og = sentance.split(" ").join("").split("").join("").toLowerCase()
+
   return og == reversed
 }
 
